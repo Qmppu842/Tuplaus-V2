6cd45556e3706ffa8ea936e111cad8ba3749cdfa
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2023-06-01 12:45:17 +0300

    Added failing test and condensed the main method.

--- src/test/kotlin/io/qmppu842/ApplicationTest.kt
+++ src/test/kotlin/io/qmppu842/ApplicationTest.kt
@@ -1,5 +1,20 @@
 package io.qmppu842
 
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import org.junit.Test
+import kotlin.test.assertEquals
+
 
 class ApplicationTest {
+
+    @Test
+    fun testConfirmation() = testApplication {
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World", bodyAsText())
+        }
+    }
 }
