f97f474def7618e7dae1669e514dd1d27cda7633
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2023-06-02 16:59:51 +0300

    Added possibility to get players last game.

--- src/main/kotlin/io/qmppu842/game/GameController.kt
+++ src/main/kotlin/io/qmppu842/game/GameController.kt
@@ -1,14 +1,18 @@
 package io.qmppu842.game
 
+import io.ktor.util.date.*
 import io.qmppu842.RandomHolder
 import io.qmppu842.player.PlayerController
 import io.qmppu842.player.PlayerData
+import kotlin.random.Random
 
 object GameController {
+    private val gameHistory = ArrayList<GameEvent>(100)
 
 
     fun playGame(player: PlayerData, bet: Int?, isItBig: Boolean): GameEvent {
         if (bet == null) {
+
             TODO("to implement the combo system")
         }
         if (player.balance - bet < 0) {
@@ -24,12 +28,35 @@ object GameController {
 //
         PlayerController.updatePlayerBalance(player.identity, winnings)
 
-        return GameEvent(
+        val ge = GameEvent(
+            id = gameHistory.size,
             playerIdentity = player.identity,
+            timestamp = getTimeMillis(),
             bet = bet,
             isPlayerChoiceBig = isItBig,
             endCard = result,
             winnings = winnings
         )
+        gameHistory.add(ge)
+
+        return ge
     }
+
+
+    fun getAllGames(): List<GameEvent> {
+        return gameHistory
+    }
+
+    fun getLastGameFrom(identity: String): GameEvent {
+        return getAllGames().mapNotNull {
+            if (it.playerIdentity == identity) {
+                it
+            } else {
+                null
+            }
+        }.maxBy { it.timestamp; it.id }
+        // huh, there actually shouldn't be any case where timestamps outperform id alone
+    }
+
+
 }
--- src/test/kotlin/io/qmppu842/game/GameControllerTest.kt
+++ src/test/kotlin/io/qmppu842/game/GameControllerTest.kt
@@ -5,7 +5,6 @@ import io.qmppu842.newPerson
 import io.qmppu842.player.PlayerController
 import io.qmppu842.player.PlayerData
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -23,6 +22,11 @@ class GameControllerTest {
         hiLoQueue.offer(false)
         hiLoQueue.offer(false)
         hiLoQueue.offer(true)
+        hiLoQueue.offer(true)
+        hiLoQueue.offer(true)
+        hiLoQueue.offer(true)//10
+
+        player = PlayerController.newPlayer(newPerson())
     }
 
     @AfterEach
@@ -31,7 +35,7 @@ class GameControllerTest {
 
     @Test
     fun playSomeGames() {
-        player = PlayerController.newPlayer(newPerson())
+        val gameEvents = GameController.getAllGames().size
 
         //Game 1
         val gameEvent = GameController.playGame(player, 10, hiLoQueue.poll())
@@ -39,22 +43,26 @@ class GameControllerTest {
         assertEquals(10, gameEvent.winnings, "Winnings don't match")
         var newPlayer = PlayerController.getPlayer(player.identity)
         assertEquals(10010, newPlayer.balance)
-
+        assertEquals(gameEvents + 1, GameController.getAllGames().size)
 
         //Game 2
-        GameController.playGame(player, 10, hiLoQueue.poll())
+        GameController.playGame(player, 350, hiLoQueue.poll())
         newPlayer = PlayerController.getPlayer(player.identity)
-        assertEquals(10020, newPlayer.balance)
+        assertEquals(10360, newPlayer.balance)
+        assertEquals(gameEvents + 2, GameController.getAllGames().size)
 
+        //Game 3 Incorrect
+        GameController.playGame(player, 150, !hiLoQueue.poll())
+        newPlayer = PlayerController.getPlayer(player.identity)
+        assertEquals(10210, newPlayer.balance)
 
-        //Game 3
+        //Game 4 Incorrect
         GameController.playGame(player, 10, !hiLoQueue.poll())
         newPlayer = PlayerController.getPlayer(player.identity)
-        assertEquals(10010, newPlayer.balance)
+        assertEquals(10200, newPlayer.balance)
 
-
-        //Game 4
-        GameController.playGame(player, 10, !hiLoQueue.poll())
+        //Game 5 Incorrect
+        GameController.playGame(player, 200, !hiLoQueue.poll())
         newPlayer = PlayerController.getPlayer(player.identity)
         assertEquals(10000, newPlayer.balance)
     }
@@ -62,8 +70,6 @@ class GameControllerTest {
 
     @Test
     fun playSomeMoreGames() {
-        player = PlayerController.newPlayer(newPerson())
-
         //Game 1
         val gameEvent = GameController.playGame(player, 10, hiLoQueue.poll())
         assertEquals(10, gameEvent.winnings, "Winnings don't match")
@@ -81,7 +87,7 @@ class GameControllerTest {
         assertTrue(didCatch, "No exception with too big bet")
 
 
-        //Game 2
+        //Game 2 Incorrect
         GameController.playGame(player, 10010, !hiLoQueue.poll())
         newPlayer = PlayerController.getPlayer(player.identity)
         assertEquals(0, newPlayer.balance)
@@ -103,8 +109,6 @@ class GameControllerTest {
 
     @Test
     fun playSomeNegGames() {
-        player = PlayerController.newPlayer(newPerson())
-
         //Game 1
         val gameEvent = GameController.playGame(player, 10, hiLoQueue.poll())
         assertEquals(10, gameEvent.winnings, "Winnings don't match")
@@ -133,6 +137,36 @@ class GameControllerTest {
         newPlayer = PlayerController.getPlayer(player.identity)
         assertEquals(10010, newPlayer.balance)
         assertTrue(didCatch, "No exception with negative bet with winning game")
+    }
+
+    @Test
+    fun findLastGames() {
+        playSomeGames()
+
+        //Game 6
+        val actualLastGame = GameController.playGame(player, 400, hiLoQueue.poll())
+        val newPlayer = PlayerController.getPlayer(player.identity)
+        assertEquals(10400, newPlayer.balance)
+
+        val gettedLastGame: GameEvent = GameController.getLastGameFrom(newPlayer.identity)
+
+        assertEquals(actualLastGame, gettedLastGame, "Last game was not what was expected")
+    }
+
+    @Test
+    fun findLastGamesMultipleUsers() {
+        playSomeGames()
+
+        //Game 6
+        val actualLastGame = GameController.playGame(player, 400, hiLoQueue.poll())
+        val newPlayer = PlayerController.getPlayer(player.identity)
+        assertEquals(10400, newPlayer.balance)
+
+        GameController.playGame(PlayerController.newPlayer(newPerson()), 456, hiLoQueue.poll())
+
+        val gettedLastGame: GameEvent = GameController.getLastGameFrom(newPlayer.identity)
+
+        assertEquals(actualLastGame, gettedLastGame, "Last game was not what was expected")
     }
 
 }
